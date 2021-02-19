8a44d69f91bea3b4eb5d8626150707e413cad6df
Abayomi Aishida <yomicorperate@gmail.com>
2021-02-19 21:54:39 +0100

    Returning chunks of array from a single array

--- index.js
+++ index.js
@@ -178,5 +178,46 @@ function longestWord(sen) {
   }
 }
 
-const output = longestWord("helloo there, how you?");
+function chunkArray(arr, len) {
+  // SOLUTION 1
+  // Init chunked arr
+  // const chunkedArr = [];
+  // // Set index
+  // let i = 0;
+
+  // // Loop while index is less than the array length
+  // while (i < arr.length) {
+  //   // Slice out from the index to the index + the chunk length and push to the chunked array
+  //   chunkedArr.push(arr.slice(i, i + len));
+  //   // console.log(i + len);
+  //   // Increment by chunk length
+  //   i += len;
+  // }
+
+  // return chunkedArr;
+
+  // SOLUTION 2
+  // Init chunked arrj
+  const chunkedArr = [];
+
+  // Loop through arr
+  arr.forEach((val) => {
+    // Get last element
+    const last = chunkedArr[chunkedArr.length - 1];
+    // console.log(val);
+
+    // Check if last and if last length is equal to the chunk len
+    if (!last || last.length === len) {
+      chunkedArr.push([val]);
+    } else {
+      last.push(val);
+    }
+
+    console.log(last);
+  });
+
+  return chunkedArr;
+}
+
+const output = chunkArray([5, 2, 3, 4, 5, 6, 7], 2);
 console.log(output);
