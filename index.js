488b1aad79e9b8ac32e050ddd68ac6920e9c195f
Abayomi Aishida <yomicorperate@gmail.com>
2021-02-19 06:59:44 +0100

    Returning the longest word

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+// JS Cadio
+// reserveString()
+// isPalindrome()
+// reverseInt()
+// maxCharacter()
+// fizzBuzz()
+
 function reverseString(str) {
   // const strArr = str.split("");
   // strArr.reverse();
@@ -150,5 +157,26 @@ function fizzBuzz() {
   }
 }
 
-const output = fizzBuzz();
+function longestWord(sen) {
+  // Create filtered array
+  const wordArr = sen.toLowerCase().match(/[a-z0-9]+/g);
+
+  // Sort by length
+  const sorted = wordArr.sort((a, b) => b.length - a.length);
+
+  // If multiple words, put into array
+  const longestWordArr = sorted.filter(
+    (word) => word.length === sorted[0].length
+  );
+
+  // Check if more than one array val
+  if (longestWordArr.length === 1) {
+    // Return the word
+    return longestWordArr[0];
+  } else {
+    return longestWordArr;
+  }
+}
+
+const output = longestWord("helloo there, how you?");
 console.log(output);
