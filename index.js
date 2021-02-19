a865189626348b25a7c9601a2f346ddef7c0565f
Abayomi Aishida <yomicorperate@gmail.com>
2021-02-19 05:58:25 +0100

    FizzBuzz logic

--- index.js
+++ index.js
@@ -136,5 +136,19 @@ function maxCharacter(str) {
   return maxChar;
 }
 
-const output = maxCharacter("javascript");
+function fizzBuzz() {
+  for (let i = 1; i <= 100; i++) {
+    if (i % 15 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+const output = fizzBuzz();
 console.log(output);
