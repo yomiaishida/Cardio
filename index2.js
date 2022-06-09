75592bb10c118a067a6c402862506640fdcd3b46
Abayomi Aishida <yomicorperate@gmail.com>
2022-06-09 23:13:14 +0100

    Add up to n

---
+++ index2.js
@@ -0,0 +1,24 @@
+// JavaScript Materclass
+// Add up to n 1
+function addUpTo(n) {
+  let total = 0;
+  for (let i = 1; i <= n; i++) {
+    total += i;
+  }
+  return total;
+}
+
+var t1 = performance.now();
+addUpTo(1000000000);
+var t2 = performance.now();
+console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds`);
+
+// Add up to n 2
+function addUpTo(n) {
+  return (n * (n + 1)) / 2;
+}
+
+var t1 = performance.now();
+addUpTo(1000000000);
+var t2 = performance.now();
+console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds`);
