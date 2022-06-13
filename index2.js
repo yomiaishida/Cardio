3d98b63800feccd060fc03c834c3708973115275
Abayomi Aishida <yomicorperate@gmail.com>
2022-06-13 22:01:53 +0100

    Problem solving approach

--- index2.js
+++ index2.js
@@ -44,3 +44,17 @@ function double(arr) {
 }
 
 double([2, 3, 4]);
+
+function charCount(str) {
+  // do something
+  // return an object with keys that are lowercase alphanumeric characters in the string; values should be the counts for those characters
+}
+
+function charCount(str) {
+  // make object to return at end
+  // loop over string, for each character...
+  // if the char is a number/letter AND is a key in object, add one to count
+  // if the char is a number/letter AND not in object, add it and set value to 1
+  // if character is something else (space, period, etc.) don't do anything
+  // return object at end
+}
