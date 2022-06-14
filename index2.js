151a855de2f97522d308b632102a619dd81b1080
Abayomi Aishida <yomicorperate@gmail.com>
2022-06-14 23:04:05 +0100

    Problem soving approach: solve/simplify

--- index2.js
+++ index2.js
@@ -52,9 +52,20 @@ function charCount(str) {
 
 function charCount(str) {
   // make object to return at end
+  var result = {};
   // loop over string, for each character...
-  // if the char is a number/letter AND is a key in object, add one to count
-  // if the char is a number/letter AND not in object, add it and set value to 1
+  for (var i = 0; i < str.length; i++) {
+    var char = str[i].toLowerCase();
+    // if the char is a number/letter AND is a key in object, add one to count
+    if (result[char] > 0) {
+      result[char]++;
+    }
+    // if the char is a number/letter AND not in object, add it and set value to 1
+    else {
+      result[char] = 1;
+    }
+  }
   // if character is something else (space, period, etc.) don't do anything
   // return object at end
+  return result;
 }
