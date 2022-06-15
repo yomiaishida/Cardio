8d1d3cc062ccf4fadca915a0b5f358d5b4cecede
Abayomi Aishida <yomicorperate@gmail.com>
2022-06-15 21:34:35 +0100

    Look back and refactor

--- index2.js
+++ index2.js
@@ -50,22 +50,45 @@ function charCount(str) {
   // return an object with keys that are lowercase alphanumeric characters in the string; values should be the counts for those characters
 }
 
+// function charCount(str) {
+//   // make object to return at end
+//   var result = {};
+//   // loop over string, for each character...
+//   for (var i = 0; i < str.length; i++) {
+//     var char = str[i].toLowerCase();
+//     // if the char is a number/letter AND is a key in object, add one to count
+//     if (result[char] > 0) {
+//       result[char]++;
+//     }
+//     // if the char is a number/letter AND not in object, add it and set value to 1
+//     else {
+//       result[char] = 1;
+//     }
+//   }
+//   // if character is something else (space, period, etc.) don't do anything
+//   // return object at end
+//   return result;
+// }
+
 function charCount(str) {
-  // make object to return at end
-  var result = {};
-  // loop over string, for each character...
-  for (var i = 0; i < str.length; i++) {
-    var char = str[i].toLowerCase();
-    // if the char is a number/letter AND is a key in object, add one to count
-    if (result[char] > 0) {
-      result[char]++;
-    }
-    // if the char is a number/letter AND not in object, add it and set value to 1
-    else {
-      result[char] = 1;
+  var obj = {};
+  for (var char of str) {
+    if (isAlphaNumeric(char)) {
+      char = char.toLowerCase();
+      obj[char] = ++obj[char] || 1;
     }
   }
-  // if character is something else (space, period, etc.) don't do anything
-  // return object at end
-  return result;
+  return obj;
+}
+
+function isAlphaNumeric(char) {
+  var code = char.charCodeAt(0);
+  if (
+    !(code > 47 && code < 58) && // numeric (0-9)
+    !(code > 64 && code < 91) && // numeric (0-9)
+    !(!code > 47 && code < 58)
+  ) {
+    return false; // lower alpha (a-z) }
+  }
+  return true;
 }
