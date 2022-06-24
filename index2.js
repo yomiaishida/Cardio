bc6156f7b8d1efbe8cbc581b803d7dec9d90d82c
Abayomi Aishida <yomicorperate@gmail.com>
2022-06-24 15:04:25 +0100

    Code refactor: isAnagram

--- index2.js
+++ index2.js
@@ -170,3 +170,32 @@ function isAnagram(a, b) {
 }
 
 isAnagram("anan", "anna");
+
+// Code Refactor: isAnagram
+function isAnagram(first, second) {
+  if (first.length !== second.length) {
+    return false;
+  }
+
+  const lookup = {};
+
+  for (let i = 0; i < first.length; i++) {
+    let letter = first[i];
+    // if letter exists, increment, otherwise set to 1
+    lookup[letter] ? (lookup[letter] += 1) : (lookup[letter] = 1);
+  }
+
+  for (let i = 0; i < second.length; i++) {
+    let letter = second[i];
+    // can't find letter or letter is zero then it's not an isAnagram
+    if (!lookup[letter]) {
+      return false;
+    } else {
+      lookup[letter] -= 1;
+    }
+  }
+
+  return true;
+}
+
+isAnagram("anagram", "nagaram");
