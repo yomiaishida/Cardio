c155ee65b38585a6e90f60712e0ad9288589ae78
Abayomi Aishida <yomicorperate@gmail.com>
2022-06-24 18:36:44 +0100

    Multiple Pointers challenge

--- index2.js
+++ index2.js
@@ -199,3 +199,34 @@ function isAnagram(first, second) {
 }
 
 isAnagram("anagram", "nagaram");
+
+// Multiple Pointers O(N^2)
+function sumZero(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === 0) {
+        return [arr[i], arr[j]];
+      }
+    }
+  }
+}
+
+sumZero([-4, -3, -2, -1, 0, 1, 2, 5]);
+
+// Multiple Pointers refactor O(N)
+function sumZero1(arr) {
+  let left = 0;
+  let right = arr.length - 1;
+  while (left < right) {
+    let sum = arr[left] + arr[right];
+    if (sum === 0) {
+      return [arr[left], arr[right]];
+    } else if (sum > 0) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+}
+
+sumZero1([-4, -3, -2, -1, 0, 3, 5, 10]);
