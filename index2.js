a047ef663a45b0888d440ebe45f5856c35c22379
Abayomi Aishida <yomicorperate@gmail.com>
2022-06-28 05:47:19 +0100

    Sliding window: maxSum refactor

--- index2.js
+++ index2.js
@@ -263,4 +263,20 @@ function maxSubarraySum(arr, num) {
   return max;
 }
 
+// max_sum refactor with Sliding Window
+function maxSubarraySum1(arr, num) {
+  let maxSum = 0;
+  let tempSum = 0;
+  if (arr.length < num) return null;
+  for (let i = 0; i < num; i++) {
+    maxSum += arr[i];
+  }
+  tempSum = maxSum;
+  for (let i = num; i < arr.length; i++) {
+    tempSum = tempSum - arr[i - num] + arr[i];
+    maxSum = Math.max(maxSum, tempSum);
+  }
+  return maxSum;
+}
+
 maxSubarraySum([2, 6, 9, 2, 1, 8, 5, 6, 3], 3);
