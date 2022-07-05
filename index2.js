4cbe4ac4ebf753b9318da77d9cf4d2f104187e47
Abayomi Aishida <yomicorperate@gmail.com>
2022-07-05 23:48:27 +0100

    Helper method recursion

--- index2.js
+++ index2.js
@@ -306,3 +306,26 @@ function factorial(num) {
 }
 
 factorial(2);
+
+// Helper Method Recursion
+function collectOddValues(arr) {
+  let result = [];
+
+  function helper(helperInput) {
+    if (helperInput.length === 0) {
+      return;
+    }
+
+    if (helperInput[0] % 2 !== 0) {
+      result.push(helperInput[0]);
+    }
+
+    helper(helperInput.slice(1));
+  }
+
+  helper(arr);
+
+  return result;
+}
+
+collectOddValues([1, 2, 3, 4, 5, 6, 7, 8, 9]);
