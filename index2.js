b3473b941eced37056f59c03c728a2ba59a3cf07
Abayomi Aishida <yomicorperate@gmail.com>
2022-07-11 23:21:26 +0100

    Pure recursive function

--- index2.js
+++ index2.js
@@ -328,4 +328,23 @@ function collectOddValues(arr) {
   return result;
 }
 
-collectOddValues([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+collectOddValues2([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+// Pure Recursion
+function collectOddValues2(arr) {
+  let newArr = [];
+
+  if(arr.length ===0) {
+    return newArr
+  }
+
+  if(arr[0] % 2 !== 0) {
+    newArr.push(arr[0]) 
+  }
+
+  newArr = newArr.concat(collectOddValues(arr.slice(1)))
+  return newArr;
+}
+
+collectOddValues2([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
