0e6ac87b13c7344641a5ee0feec25b8852d44d93
Abayomi Aishida <yomicorperate@gmail.com>
2022-07-15 22:29:11 +0100

    Bubble sort more optimization

--- index2.js
+++ index2.js
@@ -440,3 +440,24 @@ function bubbleSort(arr) {
 }
 
 bubbleSort([2,5,6,3])
+
+// Optimized Bubble Sort with noSwaps
+function bubbleSort(arr) {
+  let noSwaps;
+  const swap = (arr, idx1, idx2) => {
+      [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]
+  }
+  for (let i = arr.length; i > 0; i--) {
+      noSwaps = true
+      for (let j = 0; j < i - 1; j++) {
+          console.log(arr, arr[j], arr[j+1])
+          if(arr[j] > arr[j+1]) {
+              swap(arr, j, j+1)
+          }
+      }
+      if(noSwaps) break;
+  }
+  return arr
+}
+
+bubbleSort([2,5,6,3])
