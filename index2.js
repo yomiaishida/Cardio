15a2f2e65cde778aa3a260a90b2967183681b031
Abayomi Aishida <yomicorperate@gmail.com>
2022-08-08 22:06:04 +0100

    Radix helper functions

--- index2.js
+++ index2.js
@@ -572,3 +572,21 @@ function quickSort(arr, left = 0, right = arr.length - 1) {
 }
 
 quickSort([4, 6, 9, 1, 2, 5, 3]);
+
+// Radix Sort Helper Functions
+function getDigit(num, i) {
+  return Math.floor((Math.abs(num) / Math.pow(10, i)) % 10);
+}
+
+function digitCount(num) {
+  if (num === 0) return 1;
+  return Math.floor(Math.log10(Math.abs(num))) + 1;
+}
+
+function mostDigits(nums) {
+  let maxDigits = 0;
+  for (let i = 0; i < nums.length; i++) {
+    maxDigits = Math.max(maxDigits, digitCount(nums[i]));
+  }
+  return maxDigits;
+}
