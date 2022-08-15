70e8321831f5afac8bccb8636ef3c828f2092177
Abayomi Aishida <yomicorperate@gmail.com>
2022-08-15 21:14:28 +0100

    SinglyLinkedList get method

--- dataStructure.js
+++ dataStructure.js
@@ -69,4 +69,14 @@ class SinglyLinkedList {
     this.length += 1;
     return this;
   }
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+    var counter = 0;
+    var current = this.head;
+    while (counter !== index) {
+      current = current.next;
+      counter++;
+    }
+    return current;
+  }
 }
