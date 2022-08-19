9ada2437cc7062a7482377ca4cee0baad908acfe
Abayomi Aishida <yomicorperate@gmail.com>
2022-08-19 22:16:50 +0100

    Stack implementation

--- dataStructure.js
+++ dataStructure.js
@@ -265,3 +265,43 @@ class DoublyLinkedList {
     return deleteInd;
   }
 }
+
+// Stack implementation
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last - null;
+    this.size = 0;
+  }
+  // Implemented as in unshift method
+  push(val) {
+    var newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      var temp = this.first;
+      this.first = newNode;
+      newNode.next = temp;
+    }
+    return ++this.size;
+  }
+  // Implemented as in shift method
+  pop() {
+    if (!this.first) return null;
+    var temp = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.value;
+  }
+}
