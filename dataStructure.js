f799999284613d48de71849163cbd9f2d65a738f
Abayomi Aishida <yomicorperate@gmail.com>
2022-08-19 16:07:10 +0100

    DoublyLinkedList push and pop methods

--- dataStructure.js
+++ dataStructure.js
@@ -142,4 +142,32 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+  push(val) {
+    var newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  pop() {
+    if (!this.head) return undefined;
+    var currTail = this.tail;
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = currTail.prev;
+      this.tail.next = null;
+      currTail.prev = null;
+    }
+    this.length--;
+    return currTail;
+  }
 }
