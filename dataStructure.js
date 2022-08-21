e5c313f749020c7bacd49bcba98c23c01cffad90
Abayomi Aishida <yomicorperate@gmail.com>
2022-08-21 23:56:23 +0100

    Enqueue and dequeue  methods

--- dataStructure.js
+++ dataStructure.js
@@ -318,8 +318,28 @@ class Queue {
   constructor() {
     this.first = null;
     this.last = null;
-    this.size;
+    this.size = 0;
   }
-  enqueue(val) {}
-  dequeue() {}
+  enqueue(val) {
+    var newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    return ++this.size;
+  }
+  dequeue() {
+    if (!this.first) return null;
+
+    var temp = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.value;
+  }
 }
