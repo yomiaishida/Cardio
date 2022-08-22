b98d24e409e05e5226454bf27a72f4bfc38c3a9d
Abayomi Aishida <yomicorperate@gmail.com>
2022-08-23 00:06:59 +0100

    BInary search node class

--- dataStructure.js
+++ dataStructure.js
@@ -347,3 +347,16 @@ class Queue {
 // Intro To Trees
 // Binary tree doesn't have more than two branches
 // Every thing to the right is greater than the parent node while those to the left are smaller than the parent node
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+}
