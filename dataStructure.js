a10cb82fadd0aa8f10a7f85245cfe256b94372e0
Abayomi Aishida <yomicorperate@gmail.com>
2022-08-24 21:25:08 +0100

    Binary search tree inser method

--- dataStructure.js
+++ dataStructure.js
@@ -346,7 +346,7 @@ class Queue {
 
 // Intro To Trees
 // Binary tree doesn't have more than two branches
-// Every thing to the right is greater than the parent node while those to the left are smaller than the parent node
+// Binary Search Tree: Every thing to the right is greater than the parent node while those to the left are smaller than the parent node
 class Node {
   constructor(value) {
     this.value = value;
@@ -359,4 +359,41 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+  insert(value) {
+    var newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+    var current = this.root;
+    while (true) {
+      if (value === current.value) return undefined;
+      if (value < current.value) {
+        if (current.left === null) {
+          current.left = newNode;
+          return this;
+        }
+        current = current.left;
+      } else {
+        if (current.right === null) {
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
+      }
+    }
+  }
 }
+
+//      10
+//   5     13
+// 2  7  11  16
+
+var tree = new BinarySearchTree();
+tree.insert(10);
+tree.insert(5);
+tree.insert(13);
+tree.insert(11);
+tree.insert(2);
+tree.insert(16);
+tree.insert(7);
