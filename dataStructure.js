30a82872088f3a7f98f6b9f27e9911e5d004f8a2
Abayomi Aishida <yomicorperate@gmail.com>
2022-08-30 05:59:29 +0100

    Binary tree depth first search postOrder

--- dataStructure.js
+++ dataStructure.js
@@ -444,6 +444,16 @@ class BinarySearchTree {
     traverse(this.root);
     return data;
   }
+  dfsPostOrder() {
+    var data = [];
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      data.push(node.value);
+    }
+    traverse(this.root);
+    return data;
+  }
 }
 
 var tree = new BinarySearchTree();
