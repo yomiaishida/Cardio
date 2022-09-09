71e8fddd052a8bb43741b9de778a0bbb31d57db5
Abayomi Aishida <yomicorperate@gmail.com>
2022-09-09 22:13:10 +0100

    Hash table set method

--- dataStructure.js
+++ dataStructure.js
@@ -717,4 +717,11 @@ class HashTable {
     }
     return total;
   }
+  set(key, value) {
+    let index = this._hash(key);
+    if (!this.keyMap[index]) {
+      this.keyMap[index] = [];
+    }
+    this.keyMap[index].push([key, value]);
+  }
 }
