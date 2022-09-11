47397f8a26d9c51359d94a136ec3335e3dfc9b2d
Abayomi Aishida <yomicorperate@gmail.com>
2022-09-11 23:25:35 +0100

    Hash table get all values

--- dataStructure.js
+++ dataStructure.js
@@ -735,6 +735,19 @@ class HashTable {
     }
     return undefined;
   }
+  values() {
+    let valuesArr = [];
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (let j = 0; j < this.keyMap[i].length; j++) {
+          if (!valuesArr.includes(this.keyMap[i][j][1])) {
+            valuesArr.push(this.keyMap[i][j][1]);
+          }
+        }
+      }
+    }
+    return valuesArr;
+  }
 }
 
 let ht = new HashTable(17);
