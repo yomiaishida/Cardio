66b2090422b8c981ab4aaed28dd0e954440a463f
Abayomi Aishida <yomicorperate@gmail.com>
2022-09-14 22:37:48 +0100

    Graph traversal with BFS iterative mothed

--- dataStructure.js
+++ dataStructure.js
@@ -838,6 +838,26 @@ class Graph {
     }
     return result;
   }
+  breadthFirstIterative(start) {
+    const queue = [start];
+    const result = [];
+    const visited = {};
+    let currentVertex;
+
+    visited[start] = true;
+    while (queue.length) {
+      currentVertex = queue.shift();
+      result.push(currentVertex);
+
+      this.adjacencyList[currentVertex].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          queue.push(neighbor);
+        }
+      });
+    }
+    return result;
+  }
 }
 
 var g = new Graph();
