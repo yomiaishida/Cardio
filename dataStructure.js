6ca5fce698f5e866d48e721bbadbdcfe03dead41
Abayomi Aishida <yomicorperate@gmail.com>
2022-09-14 22:14:19 +0100

    Graph traversal with DFS iterative mothed

--- dataStructure.js
+++ dataStructure.js
@@ -818,6 +818,26 @@ class Graph {
 
     return result;
   }
+  depthFirstIterative(start) {
+    const stack = [start];
+    const result = [];
+    const visited = {};
+    let currentVertex;
+
+    visited[start] = true;
+    while (stack.length) {
+      currentVertex = stack.pop();
+      result.push(currentVertex);
+
+      this.adjacencyList[currentVertex].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          stack.push(neighbor);
+        }
+      });
+    }
+    return result;
+  }
 }
 
 var g = new Graph();
