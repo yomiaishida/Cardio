afa913198bf63eb492c9955676238dc65160cff9
Abayomi Aishida <yomicorperate@gmail.com>
2022-09-16 23:19:47 +0100

    Dijkstra's algorithm part-1

--- dataStructure.js
+++ dataStructure.js
@@ -875,7 +875,22 @@ g.addEdge("D", "E");
 g.addEdge("D", "F");
 g.addEdge("E", "F");
 
-// Weighted Graph
+class PriorityQueue {
+  constructor() {
+    this.values = [];
+  }
+  enqueue(val, priority) {
+    this.values.push({ val, priority });
+    this.sort();
+  }
+  dequeue() {
+    return this.values.shift();
+  }
+  sort() {
+    this.values.sort((a, b) => a.priority - b.priority);
+  }
+}
+
 class WeightedGraph {
   constructor() {
     this.adjacencyList = {};
@@ -887,6 +902,37 @@ class WeightedGraph {
     this.adjacencyList[vertex1].push({ node: vertex2, weight });
     this.adjacencyList[vertex2].push({ node: vertex1, weight });
   }
+  Dijkstra(start, finish) {
+    const nodes = new PriorityQueue();
+    const distances = {};
+    const previous = {};
+    let smallest;
+
+    // build initial state
+    for (let vertex in this.adjacencyList) {
+      if (vertex === start) {
+        distances[vertex] = 0;
+        nodes.enqueue(vertex, 0);
+      } else {
+        distances[vertex] = Infinity;
+        nodes.enqueue(vertex, Infinity);
+      }
+      previous[vertex] = null;
+    }
+    // as long as there is something to visit
+    while (nodes.values.length) {
+      smallest = nodes.dequeue().val;
+      if (smallest === finish) {
+        // WE ARE DONE
+        // BUILD UP PATH TO RETURN AT END
+      }
+      if (smallest || distances[smallest] !== Infinity) {
+        for (let neigbor in this.adjacencyList[smallest]) {
+          // find neigboring node
+        }
+      }
+    }
+  }
 }
 
 var g = new WeightedGraph();
@@ -896,10 +942,12 @@ g.addVertex("C");
 g.addVertex("D");
 g.addVertex("E");
 g.addVertex("F");
-g.addEdge("A", "B", 3);
+
+g.addEdge("A", "B", 4);
 g.addEdge("A", "C", 2);
-g.addEdge("B", "D", 1);
-g.addEdge("C", "E", 6);
-g.addEdge("D", "E", 5);
-g.addEdge("D", "F", 7);
-g.addEdge("E", "F", 8);
+g.addEdge("B", "E", 3);
+g.addEdge("C", "D", 2);
+g.addEdge("C", "F", 4);
+g.addEdge("D", "E", 3);
+g.addEdge("D", "F", 1);
+g.addEdge("E", "F", 1);
