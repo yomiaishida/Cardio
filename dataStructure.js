f7434486bdf218563f016fa25787991acecc4fb9
Abayomi Aishida <yomicorperate@gmail.com>
2022-09-21 21:08:56 +0100

    Memoization

--- dataStructure.js
+++ dataStructure.js
@@ -1115,3 +1115,12 @@ g.addEdge("D", "F", 1);
 g.addEdge("E", "F", 1);
 
 g.Dijkstra();
+
+// Memoization With Fibonacci Sequence
+function fib(n, memo = []) {
+  if (memo[n] !== undefined) return memo[n];
+  if (n <= 2) return 1;
+  var res = fib(n - 1, memo) + fib(n - 2, memo);
+  memo[n] = res;
+  return res;
+}
