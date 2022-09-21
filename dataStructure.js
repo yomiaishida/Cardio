659464c891e0fbdb7ca25ba7bce61962e81df196
Abayomi Aishida <yomicorperate@gmail.com>
2022-09-21 21:38:08 +0100

    Fibanacci Tabular: A bottom up approach

--- dataStructure.js
+++ dataStructure.js
@@ -1124,3 +1124,13 @@ function fib(n, memo = []) {
   memo[n] = res;
   return res;
 }
+
+// Tabulation A Bottom Up Approach
+function fib_table(n) {
+  if (n <= 2) return 1;
+  var fibNums = [0, 1, 1];
+  for (var i = 3; i <= n; i++) {
+    fibNums[i] = fibNums[i - 1] + fibNums[i - 2];
+  }
+  return fibNums[n];
+}
