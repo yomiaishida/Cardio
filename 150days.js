09b1636843d2cbe594f2de04d52f3d44a106784d
Abayomi Aishida <yomicorperate@gmail.com>
2023-01-01 23:50:03 +0100

    Day 5: Best time to buy and sell stock

--- 150days.js
+++ 150days.js
@@ -131,3 +131,37 @@ var isPalindrome = function(s) {
   
 return revStr === newS;
 };
+
+// you are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+// Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+ 
+
+// Example 1:
+
+// Input: prices = [7,1,5,3,6,4]
+// Output: 5
+// Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+// Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+// Example 2:
+
+// Input: prices = [7,6,4,3,1]
+// Output: 0
+// Explanation: In this case, no transactions are done and the max profit = 0.
+var maxProfit = function (prices) {
+  let min = prices[0];
+  let max = min;
+  let value = 0;
+  for (let i = 0; i < prices.length; i++) {
+      if (i != prices.length - 1 && prices[i] <= min) {
+          max = min = prices[i];
+      } else if (prices[i] > max) {
+          max = prices[i];
+      }
+      value = max - min > value ? max - min : value;
+  }
+  return value;
+};
