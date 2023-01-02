0a0ed8c14fe144e9b09e7275344c496c7713751b
Abayomi Aishida <yomicorperate@gmail.com>
2023-01-02 23:58:49 +0100

    Day 6: Valid Parentheses

--- 150days.js
+++ 150days.js
@@ -165,3 +165,27 @@ var maxProfit = function (prices) {
   }
   return value;
 };
+
+// Day 6
+var isValid = (s, stack = []) => {
+  for (const bracket of s.split('')) {/* Time O(N) */
+      const isParenthesis = bracket === '(';
+      if (isParenthesis) stack.push(')');  /* Space O(N) */
+
+      const isCurlyBrace = bracket === '{';
+      if (isCurlyBrace) stack.push('}');   /* Space O(N) */
+
+      const isSquareBracket = bracket === '[';
+      if (isSquareBracket) stack.push(']');/* Space O(N) */
+
+      const isOpenPair = isParenthesis || isCurlyBrace || isSquareBracket;
+      if (isOpenPair) continue;
+
+      const isEmpty = !stack.length;
+      const isWrongPair = stack.pop() !== bracket;
+      const isInvalid = isEmpty || isWrongPair;
+      if (isInvalid) return false;
+  }
+
+  return (stack.length === 0);
+};
