8827616a3fe1e788e70b8665b0b149a48c55d34d
Abayomi Aishida <yomicorperate@gmail.com>
2023-01-05 23:48:38 +0100

    Day 8: Reverse Linked List(correct solution)

--- 150days.js
+++ 150days.js
@@ -12,28 +12,24 @@
 // Input: nums = [1,1,1,3,3,4,3,2,4,2]
 // Output: true
 
-var containsDuplicate = function(nums) {
-   
-    let sortedArr = nums.sort((a, b) => a-b)
-    console.log(sortedArr)
-    let val;
+var containsDuplicate = function (nums) {
+  let sortedArr = nums.sort((a, b) => a - b);
+  console.log(sortedArr);
+  let val;
 
-    while(sortedArr.length > 0) {
-        
-        if(val === sortedArr[sortedArr.length-1]) {
-            return true
-        }
-        val = sortedArr.pop()
-        console.log(sortedArr)
+  while (sortedArr.length > 0) {
+    if (val === sortedArr[sortedArr.length - 1]) {
+      return true;
     }
-        return false
+    val = sortedArr.pop();
+    console.log(sortedArr);
+  }
+  return false;
 };
 
 // Given two strings s and t, return true if t is an anagram of s, and false otherwise.
 
 // An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
-
- 
 
 // Example 1:
 
@@ -43,8 +39,8 @@ var containsDuplicate = function(nums) {
 
 // Input: s = "rat", t = "car"
 // Output: false
-var isAnagram = function(s, t) {
-    let arr1 = {};
+var isAnagram = function (s, t) {
+  let arr1 = {};
   let arr2 = {};
 
   for (let val of s) {
@@ -75,8 +71,6 @@ var isAnagram = function(s, t) {
 
 // You can return the answer in any order.
 
- 
-
 // Example 1:
 
 // Input: nums = [2,7,11,15], target = 9
@@ -90,21 +84,19 @@ var isAnagram = function(s, t) {
 
 // Input: nums = [3,3], target = 6
 // Output: [0,1]
-var twoSum = function(nums, target) {
-    for(let i = 0; i < nums.length; i++) {
-        for(let j = 1; j < nums.length; j++) {
-            if(nums[i] + nums[j] === target && i !== j) {
-                return [i, j]
-            }
-        }
+var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target && i !== j) {
+        return [i, j];
+      }
     }
+  }
 };
 
 // A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
 
 // Given a string s, return true if it is a palindrome, or false otherwise.
-
- 
 
 // Example 1:
 
@@ -122,14 +114,14 @@ var twoSum = function(nums, target) {
 // Output: true
 // Explanation: s is an empty string "" after removing non-alphanumeric characters.
 // Since an empty string reads the same forward and backward, it is a palindrome.
-var isPalindrome = function(s) {
-  let newS = s.toLowerCase()
-  let nonAlphanum = /[^a-z0-9]/g
-  newS = newS.replace(nonAlphanum, "")
+var isPalindrome = function (s) {
+  let newS = s.toLowerCase();
+  let nonAlphanum = /[^a-z0-9]/g;
+  newS = newS.replace(nonAlphanum, "");
 
-  const revStr = newS.split("").reverse().join("")
-  
-return revStr === newS;
+  const revStr = newS.split("").reverse().join("");
+
+  return revStr === newS;
 };
 
 // you are given an array prices where prices[i] is the price of a given stock on the ith day.
@@ -137,8 +129,6 @@ return revStr === newS;
 // You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
 
 // Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
-
- 
 
 // Example 1:
 
@@ -156,44 +146,53 @@ var maxProfit = function (prices) {
   let max = min;
   let value = 0;
   for (let i = 0; i < prices.length; i++) {
-      if (i != prices.length - 1 && prices[i] <= min) {
-          max = min = prices[i];
-      } else if (prices[i] > max) {
-          max = prices[i];
-      }
-      value = max - min > value ? max - min : value;
+    if (i != prices.length - 1 && prices[i] <= min) {
+      max = min = prices[i];
+    } else if (prices[i] > max) {
+      max = prices[i];
+    }
+    value = max - min > value ? max - min : value;
   }
   return value;
 };
 
 // Day 6
 var isValid = (s, stack = []) => {
-  for (const bracket of s.split('')) {/* Time O(N) */
-      const isParenthesis = bracket === '(';
-      if (isParenthesis) stack.push(')');  /* Space O(N) */
+  for (const bracket of s.split("")) {
+    /* Time O(N) */
+    const isParenthesis = bracket === "(";
+    if (isParenthesis) stack.push(")"); /* Space O(N) */
 
-      const isCurlyBrace = bracket === '{';
-      if (isCurlyBrace) stack.push('}');   /* Space O(N) */
+    const isCurlyBrace = bracket === "{";
+    if (isCurlyBrace) stack.push("}"); /* Space O(N) */
 
-      const isSquareBracket = bracket === '[';
-      if (isSquareBracket) stack.push(']');/* Space O(N) */
+    const isSquareBracket = bracket === "[";
+    if (isSquareBracket) stack.push("]"); /* Space O(N) */
 
-      const isOpenPair = isParenthesis || isCurlyBrace || isSquareBracket;
-      if (isOpenPair) continue;
+    const isOpenPair = isParenthesis || isCurlyBrace || isSquareBracket;
+    if (isOpenPair) continue;
 
-      const isEmpty = !stack.length;
-      const isWrongPair = stack.pop() !== bracket;
-      const isInvalid = isEmpty || isWrongPair;
-      if (isInvalid) return false;
+    const isEmpty = !stack.length;
+    const isWrongPair = stack.pop() !== bracket;
+    const isInvalid = isEmpty || isWrongPair;
+    if (isInvalid) return false;
   }
 
-  return (stack.length === 0);
+  return stack.length === 0;
 };
 
 // Day 8 Reverse Linked List
 var reverse = (head) => {
-  let newHead = head[head.length-1]
-  let newTail = head[0]
-  let next;
-  let prev = null;
-}
+  let [prev, curr, next] = [null, head, null];
+
+  while (curr) {
+    /* Time O(N) */
+    next = curr.next;
+    curr.next = prev;
+
+    prev = curr;
+    curr = next;
+  }
+
+  return prev;
+};
