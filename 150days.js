64e74df39a81fbfdd95c897e47b33a41f4f3f0b5
Abayomi Aishida <yomicorperate@gmail.com>
2023-01-07 23:36:28 +0100

    Day 9: Merge two sorted lists

--- 150days.js
+++ 150days.js
@@ -212,3 +212,30 @@ var reverse = (head) => {
 
   return prev;
 };
+
+// Day 9 Merge Two Sorted Lists
+var mergeTwoLists = function (list1, list2) {
+  let sentinel = (tail = new ListNode());
+
+  while (list1 && list2) {
+    /* Time O(N + M) */
+    const isL2Greater = list1.val <= list2.val;
+    const isL2Less = list2.val < list1.val;
+
+    if (isL2Greater) {
+      tail.next = list1;
+      list1 = list1.next;
+    }
+
+    if (isL2Less) {
+      tail.next = list2;
+      list2 = list2.next;
+    }
+
+    tail = tail.next;
+  }
+
+  tail.next = list1 || list2;
+
+  return sentinel.next;
+};
